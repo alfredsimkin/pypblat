52072c154aec294472a5d1e85910257a0f6b4bc8
Alfred Simkin <alfred.simkin@gmail.com>
2020-07-02 13:42:36 -0400

    added expected_unique, RPU

--- pypblat/te_stats.py
+++ pypblat/te_stats.py
@@ -19,20 +19,29 @@ def windowed(seq, n=2):
 class ReferenceStatistics:
     def __init__(self):
         self.unique_windows = {}
+        self.unique_counts = {}
         self.non_unique_windows = {}
         # Dict[TE Name, int]
         self.theoretical_counts = {}
 
+def parse_unique(unique_windows):
+    ref_counts={}
+    for seq in unique_windows:
+        for ref in unique_windows[seq]:
+            if ref not in ref_counts:
+                ref_counts[ref]=0
+            ref_counts[ref]+=1
+#    print(ref_counts)
+    return ref_counts
 
 def make_reference_statistics(read_len: int, path: Path, seq_format: str = "fasta") -> ReferenceStatistics:
 
     theoretical_counts = defaultdict(int)
     stats = ReferenceStatistics()
-    all_windows = ((''.join(window), record.id)
+    all_windows = (((''.join(window)).upper(), record.id)
                    for record in SeqIO.parse(str(path), seq_format)
                    for window in windowed(record.seq, read_len))
     sorted_windows = sorted(all_windows, key=lambda x: x[0])
-
     for seq, seq_ids in groupby(sorted_windows, key=lambda x: x[0]):
 
         seq_ids = list(x[1] for x in seq_ids)
@@ -45,6 +54,7 @@ def make_reference_statistics(read_len: int, path: Path, seq_format: str = "fast
             continue
 
         stats.non_unique_windows[seq] = seq_ids
-
+    stats.unique_counts=parse_unique(stats.unique_windows)
     stats.theoretical_counts = dict(theoretical_counts)
+    print(stats.theoretical_counts)
     return stats
